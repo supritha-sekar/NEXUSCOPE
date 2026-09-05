a9df1b64dd2349923124bb4db6e3e25325acb4de
Supritha Sekar <suprithasekar07@gmail.com>
2026-09-05 14:53:17 +0530

    Delete STOP_NEXUSCOPE.bat

--- STOP_NEXUSCOPE.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Stop NEXUSCOPE
-echo Stopping NEXUSCOPE processes...
-taskkill /FI "WINDOWTITLE eq NEXUSCOPE Backend*" /T /F >nul 2>nul
-taskkill /FI "WINDOWTITLE eq NEXUSCOPE Frontend*" /T /F >nul 2>nul
-echo Done.
-pause
